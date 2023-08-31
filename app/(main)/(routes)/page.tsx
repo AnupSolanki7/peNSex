f88fa2ca08a1e6966c253eb1af17f3697e3deb78
AnupWiseSkulls7 <anup@wiseskulls.com>
2023-08-31 17:06:40 +0530

    anup

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div >
+      <UserButton afterSignOutUrl="/">logout</UserButton>
+      hello
+      <ModeToggle/>
+    </div>
+  );
+}
